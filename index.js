f01cdc6b32091a1db3a5b74e71ee7da13b7e2820
Bob3x <bob3xx@protonmail.com>
2024-08-28 17:52:02 +0300

    Adding routes with Express

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 const express = require('express');
 const app = express();
+
+let topThreeMovies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+    director: 'J.K. Rowling'
+  },
+  {
+    title: 'Lord of the Rings',
+    director: 'J.R.R. Tolkien'
+  },
+  {
+    title: 'Twilight',
+    director: 'Stephanie Meyer'
+  }
+];
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to my movie app!');
+});
+
+app.get('/documentation', (req, res) => {                  
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/books', (req, res) => {
+  res.json(topBooks);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
