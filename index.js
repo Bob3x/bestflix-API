c9f534572bb03fa76da1e46fa198bcfc65123894
Bob3x <bob3xx@protonmail.com>
2024-09-05 15:56:11 +0300

    Updated movies list and information

--- index.js
+++ index.js
@@ -4,19 +4,62 @@ const path = require('path');
 
 const app = express();
 
-const topThreeMovies = [
-  {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    director: 'J.K. Rowling'
-  },
-  {
-    title: 'Lord of the Rings',
-    director: 'J.R.R. Tolkien'
-  },
-  {
-    title: 'Twilight',
-    director: 'Stephanie Meyer'
-  }
+const topMovies = [
+    {
+      title: 'The Godfather',
+      description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
+      imageURL: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
+      director: 'Francis Ford Coppola',
+      genre: {
+        name: 'Crime',
+        description:
+          'Crime movies are movies that focus on criminal activities.',
+      }
+    },
+    {
+      title: 'The Shawshank Redemption',
+      description: 'A Maine banker convicted of the murder of his wife and her lover in 1947 gradually forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.',
+      imageURL: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i',
+      director: 'Frank Darabont',
+      genre: {
+        name: 'Drama',
+        description:
+          'Crime movies are movies that focus on criminal activities.',
+      }
+    },
+    {
+      title: 'Schindler`s List',
+      description: 'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
+      imageURL: 'https://www.imdb.com/title/tt0108052/mediaviewer/rm1610023168/?ref_=tt_ov_i',
+      director: 'Steven Spielberg',
+      genre: {
+        name: 'Drama',
+        description:
+          'Crime movies are movies that focus on criminal activities.',
+      }
+    },
+    {
+      title: 'Raging Bull',
+      description: 'The life of boxer Jake LaMotta, whose violence and temper that led him to the top in the ring destroyed his life outside of it.',
+      imageURL: 'https://www.imdb.com/title/tt0081398/mediaviewer/rm3879544320/?ref_=tt_ov_i',
+      director: 'Martin Scorsese',
+      genre: {
+        name: 'Drama',
+        description:
+          'Crime movies are movies that focus on criminal activities.',
+      }
+    },
+    {
+      title: 'Citizen Kane',
+      description: 'Following the death of publishing tycoon Charles Foster Kane, reporters scramble to uncover the meaning of his final utterance: "Rosebud".',
+      imageURL: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
+      director: 'Orson Welles',
+      genre: {
+        name: 'Drama',
+        description:
+          'Crime movies are movies that focus on criminal activities.',
+      }
+    },
 ];
 
 
@@ -30,7 +73,7 @@ app.get('/documentation', (req, res) => {
 });
 
 app.get('/books', (req, res) => {
-  res.json(topBooks);
+  res.json(topMovies);
 });
 
 // Error-handling middleware
