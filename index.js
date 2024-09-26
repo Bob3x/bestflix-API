e38956ba514d121fb1c75c8ab264bdaa50de7ae7
Bob3x <bob3xx@protonmail.com>
2024-09-26 15:25:06 +0300

    Fixing some of the endpoints.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const uuid = require('uuid');
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded ({ extended: true })); 
 app.use(morgan('combined'));        // Morgan middleware to log all requests to the terminal
 app.use(express.static('public'));  // Serve static files from the "public" directory
 
@@ -19,64 +19,6 @@ const Genres = Models.Genre;
 const Directors = Models.Director;
 
 mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// List  of users
-let users = [
-  {
-    id: 1,
-    name: "Bobb",
-    favoriteMovies: []
-  },
-  {
-    id: 2,
-    name: "Sara",
-    favoriteMovies: ["The Gotfather"]
-  }
-]
-
-// List of movies
-let topMovies = [
-    {
-      title: 'The Godfather',
-      description: 'The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.',
-      imageURL: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
-      director: 'Francis Ford Coppola',
-      genre: 'Crime',
-      year: '1972'
-    },
-    {
-      title: 'The Shawshank Redemption',
-      description: 'A Maine banker convicted of the murder of his wife and her lover in 1947 gradually forms a friendship over a quarter century with a hardened convict, while maintaining his innocence and trying to remain hopeful through simple compassion.',
-      imageURL: 'https://www.imdb.com/title/tt0111161/mediaviewer/rm1690056449/?ref_=tt_ov_i',
-      director: 'Frank Darabont',
-      genre: 'Drama',
-      year: '1994'
-    },
-    {
-      title: 'Schindler`s List',
-      description: 'In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.',
-      imageURL: 'https://www.imdb.com/title/tt0108052/mediaviewer/rm1610023168/?ref_=tt_ov_i',
-      director: 'Steven Spielberg',
-      genre: 'Drama',
-      year: '1993'
-    },
-    {
-      title: 'Raging Bull',
-      description: 'The life of boxer Jake LaMotta, whose violence and temper that led him to the top in the ring destroyed his life outside of it.',
-      imageURL: 'https://www.imdb.com/title/tt0081398/mediaviewer/rm3879544320/?ref_=tt_ov_i',
-      director: 'Martin Scorsese',
-      genre: 'Drama',
-      year: '1980'
-    },
-    {
-      title: 'Citizen Kane',
-      description: 'Following the death of publishing tycoon Charles Foster Kane, reporters scramble to uncover the meaning of his final utterance: "Rosebud".',
-      imageURL: 'https://www.imdb.com/title/tt0068646/mediaviewer/rm746868224/?ref_=tt_ov_i',
-      director: 'Orson Welles',
-      genre: 'Drama',
-      year: '1941'
-    },
-];
 
 // GET request welcome url
 app.get('/', (req, res) => {
@@ -114,9 +56,9 @@ app.get('/movies/:Title', async (req, res) => {
 
 // GET movie by director's name
 app.get('/movies/Director/:Name', async (req, res) => {
-	await Directors.findOne({ Director:{ Name: req.params.Name} }) 
-  .then ((Director) => {
-    res.json(Director)
+	await Movies.findOne({ 'Director.Name': req.params.Name }) 
+  .then ((movie) => {
+    res.json(movie.Director)
   })
   .catch((err) => {
     console.error(err);
@@ -126,7 +68,7 @@ app.get('/movies/Director/:Name', async (req, res) => {
 
 // GET movie by genre
 app.get('/movies/Genre/:Name', async (req, res) => {
-	await Genres.findOne({ Name: req.params.Name })
+	await Genres.findOne({ 'Genre.Name': req.params.Name })
   .then ((Genre) => {
     res.json(Genre)
   })
@@ -136,6 +78,7 @@ app.get('/movies/Genre/:Name', async (req, res) => {
   });
 });
 
+// GET all users
 app.get('/users', async (req, res) => {
   await Users.find()
   .then ((users) => {
@@ -147,7 +90,7 @@ app.get('/users', async (req, res) => {
   });
 });
 
-// Create new user in myFlixDB - MongoDB
+// CREATE new user in myFlixDB - MongoDB
 app.post('/users', async (req, res) => {
   await Users.findOne({ Username: req.body.Username })
     .then((user) => {
@@ -166,7 +109,7 @@ app.post('/users', async (req, res) => {
           .then((user) => {res.status(201).json(user)})
           .catch((error) => {
             console.error(error);
-            res.status(500).send('User already exists.' + error);
+            res.status(500).send('Unable to create user - data is required!' + error);
           })
       }
     })
@@ -176,7 +119,7 @@ app.post('/users', async (req, res) => {
     });
 });
 
-// Update user's info by username
+// UPDATE user's info by username
 app.put('/users/:Username', async (req, res) => {
   await Users.findOneAndUpdate({Username: req.params.Username}, {
     $set:
@@ -217,7 +160,7 @@ app.delete('/users/:Username', async (req, res) => {
 // UPDATE users favorite movies
 app.post('/users/:Username/movies/:MovieID', async (req, res) => {
 await Users.findOneAndUpdate({Username: req.params.Username}, {
-  $push: { favoriteMovies: req.params.MovieID }
+  $push: { FavoriteMovies: req.params.MovieID }
 },
 {new: true})
 .then((updatedUser) => {
