7ae2808cb2b3a85ae1c6d628ec0fa82363194165
Bob3x <bob3xx@protonmail.com>
2024-10-04 14:34:45 +0300

    Fixing functions

--- index.js
+++ index.js
@@ -68,159 +68,165 @@ app.get('/movies/:Title', async (req, res) => {
 
 // GET movie by director's name
 app.get('/movies/Director/:Name', async (req, res) => {
-	await Movies.findOne({ 'Director.Name': req.params.Name }) 
-  .then ((movie) => {
-    res.json(movie.Director)
-  })
-  .catch((err) => {
+  try {
+    const movie = await Movies.findOne({ 'Director.Name': req.params.Name });
+    if (movie) {
+      res.json(movie);
+    } else {
+      res.status(404).send('Director not found.');
+    }
+  } catch (err) {
     console.error(err);
-    res.status(400).send('Director not found.' + err);
-  });
+    res.status(500).send('Error: ' + err);
+  }
 });
 
 // GET movie by genre
 app.get('/movies/Genre/:Name', async (req, res) => {
-	await Movies.findOne({ 'Genre.Name': req.params.Name })
-  .then ((Genre) => {
-    res.json(Genre)
-  })
-  .catch ((err) => {
+  try {
+    const movie = await Movies.findOne({ 'Genre.Name': req.params.Name });
+    if (movie) {
+      res.json(movie);
+    } else {
+      res.status(404).send('Genre not found.');
+    }
+  } catch (err) {
     console.error(err);
-    res.status(400).send('Genre not found.' + err);
-  });
+    res.status(500).send('Error: ' + err);
+  }
 });
 
 // GET all users
 app.get('/users', async (req, res) => {
-  await Users.find()
-  .then ((users) => {
+  try {
+    const users = await Users.find();
     res.status(200).json(users);
-  })
-  .catch((err) => {
-  console.error(err);
-  res.status(500).send('Error: ' + err);
-  });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error: ' + err);
+  }
 });
 
 // CREATE new user in myFlixDB - MongoDB
 app.post('/users', [
-  check('Username', 'Username is required.').isLength({min: 5}),
+  check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-  check('Password', 'Password is required.').not().isEmpty(),
-  check('Email', 'Email is not valid.').isEmail()
+  check('Password', 'Password is required').not().isEmpty(),
+  check('Email', 'Email is not valid').isEmail()
 ], async (req, res) => {
-  let errors = validationResult(req);
+  const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array()} );
+    return res.status(422).json({ errors: errors.array() });
   }
-  let hashedPassword = Users.hashPassword(req.body.Password);
-  await Users.findOne({ 'Username': req.body.Username })
-    .then((user) => {
-      if (user) {
-        return res.status(400).send(req.body.Username + 'already exists');
-        // If the user already exists 
-      } else {
-        Users
-        // If not - create new user
-          .create({                                                        
-            Username: req.body.Username,
-            Password: hashedPassword,
-            Email: req.body.Email,
-            Birthday: req.body.Birthday
-          })
-          .then((user) => {res.status(201).json(user)})
-          .catch((error) => {
-            console.error(error);
-            res.status(500).send('Unable to create user - data is required!' + error);
-          })
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(400).send('Bad request.' + error);
-    });
+
+  const hashedPassword = Users.hashPassword(req.body.Password);
+  try {
+    const user = await Users.findOne({ Username: req.body.Username });
+    if (user) {
+      // If the user already exists 
+      return res.status(400).send(req.body.Username + ' already exists');
+    } else {
+      // If not - create new user
+      const newUser = await Users.create({
+        Username: req.body.Username,
+        Password: hashedPassword,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      });
+      res.status(201).json(newUser);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  }
 });
 
 // UPDATE user's info by username
-app.put('/users/:Username',
-  check('Username', 'Username is required.').isLength({min: 5}),
+app.put('/users/:Username', [
+  check('Username', 'Username is required').isLength({min: 5}),
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-  check('Password', 'Password is required.').not().isEmpty(), 
-  async (req, res) => {
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(422).json({ errors: errors.array()} );
-    }
-    let hashedPassword = Users.hashPassword(req.body.Password);
-  await Users.findOneAndUpdate({ 'Username': req.params.Username }, {
-    $set:
-    {
-      Username: req.body.Username,
-      Email: req.body.Email,
-      Password: hashedPassword,
-      Birthday: req.body.Birthday
-    }
-  },
-  // Updated document is returned
-  {new: true})       
-  .then((updatedUser) => {
+  check('Password', 'Password is required').not().isEmpty(),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  const hashedPassword = Users.hashPassword(req.body.Password);
+  try {
+    const updatedUser = await Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Email: req.body.Email,
+          Password: hashedPassword,
+          Birthday: req.body.Birthday
+        }
+      },
+      { new: true } // Updated document is returned
+    );
     res.json(updatedUser);
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error(error);
-    res.status(400).send('Username not valid.' + error);
-  });
+    res.status(500).send('Error: ' + error);
+  }
 });
 
 // DELETE user by username
 app.delete('/users/:Username', async (req, res) => {
-  await Users.findOneAndRemove({'Username': req.params.Username})
-  .then((user) => {
-    if(!user) {
-      res.status(400).send(req.params.Username + 'was not found.');
+  try {
+    const user = await Users.findOneAndRemove({ Username: req.params.Username });
+    if (!user) {
+      res.status(404).send(req.params.Username + ' was not found');
     } else {
-      res.status(200).send(req.params.Username + 'has been deleted.');
+      res.status(200).send(req.params.Username + ' has been deleted');
     }
-  })
-  .catch((error) => {
+  } catch (error) {
     console.error(error);
-    res.status(500).send('No such user.' + error);
-  });
+    res.status(500).send('Error: ' + error);
+  }
 });
 
 // UPDATE users favorite movies
 app.post('/users/:Username/movies/:MovieID', async (req, res) => {
-await Users.findOneAndUpdate({ 'Username': req.params.Username}, {
-  $push: { FavoriteMovies: req.params.MovieID }
-},
-{new: true})
-.then((updatedUser) => {
-  res.json(updatedUser)
-})
-.catch((error) => {
-  console.error(error);
-  res.status(500).send('No such movie' + error);
-  }); 
+  try {
+    const updatedUser = await Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      { $push: { FavoriteMovies: req.params.MovieID } },
+      { new: true }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  }
 });
 
 // DELETE favorite movie
 app.delete('/users/:Username/movies/:MovieID', async (req, res) => {
-await Users.findOneAndUpdate({Username: req.params.Username}, {
-  $pull: { FavoriteMovies: req.params.MovieID }
-},
-{new: true})
-.then((updatedUser) => {
-  res.json(updatedUser)
-})
-.catch((error) => {
-  console.error(error);
-  res.status(500).send('Movie not found' + error);
-  }); 
+  try {
+    const updatedUser = await Users.findOneAndUpdate(
+      { Username: req.params.Username },
+      { $pull: { FavoriteMovies: req.params.MovieID } },
+      { new: true }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 // listen for requests
 const port = process.env.PORT || 8080;
-app.listen(port, '0 0 0 0', () => {
+
+app.listen(port, () => {
   console.log('Listening on port ' + port);
 });
