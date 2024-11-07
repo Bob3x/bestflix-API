ee818db65d10492789123e2736389d90d425b48e
Bob3x <bob3xx@protonmail.com>
2024-11-07 21:28:06 +0200

    Improving main code and structure

--- index.js
+++ index.js
@@ -1,74 +1,21 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const uuid = require('uuid');
 const passport = require('passport');
 const { check, validationResult } = require('express-validator');
 
-const cors = require('cors');
-// Database
-const mongoose = require('mongoose');
-const Models = require('./models.js');
-
 const app = express();
 
-const Movies = Models.Movie;
-const Users = Models.User;
+// Middleware import
+require ('./middleware')(app);
 
-// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const username = encodeURIComponent('bob3x');
-const password = encodeURIComponent('lEX6W7KWRvmEKiTL');
-const uri = `mongodb+srv://${username}:${password}@my-db-cluster.2utmo.mongodb.net/myFlixDB?retryWrites=true&w=majority&appName=my-DB-cluster`;
-
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
-
-// Middleware configurations  
-  app.use(express.json());
-  app.use(express.urlencoded ({ extended: true }));
-  app.use(morgan('combined'));        // Morgan middleware to log all requests to the terminal
-  app.use(express.static('public'));  // Serve static files from the "public" directory
-  
-// CORS configuration
-const allowedOrigins = [
-  'http://localhost:8080',
-  'http://localhost:1234',
-  'https://my-movies-flix-app-56f9661dc035.herokuapp.com/', 
-  'https://my-movie-flix.netlify.app'
-];
-
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin) 
-      return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS policy: Origin not allowed'), false);
-    }
-    return callback(null, true);
-  },
-  credentials: true,
-  methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-  allowedHeaders:["Content-Type", "Authorization"]
-};
-app.use(cors(corsOptions));
+// Database import
+const { Movies, Users } = require('./database');
 
 // Authentication
 require('./passport.js');
-let auth = require('./auth.js')(app); 
+require('./auth.js')(app); 
 
+app.use(passport.initialize());
 
-// require('dotenv').config();
-// Check if JWT_SECRET is set
-/*if (!process.env.JWT_SECRET) {
-  console.error('FATAL ERROR: JWT_SECRET is not defined.');
-  process.exit(1); // Exit the application
-}*/
-
-
-// ROUTES
 // GET request welcome url
 app.get('/', (req, res) => {
   res.send('Welcome to my movie app!');
@@ -135,6 +82,7 @@ app.get('/movies/Genre/:Name', passport.authenticate('jwt', {session: false}), a
   }
 });
 
+
 // GET all users
 app.get('/users', async (req, res) => {
   try {
@@ -165,18 +113,19 @@ app.post('/users', [
     if (user) {
       // If the user already exists 
       return res.status(400).send(req.body.Username + ' already exists');
-    } 
-      // create new user
+    } else {
+      // If not - create new user
       const newUser = await Users.create({
         Username: req.body.Username,
         Password: hashedPassword,
-        Email: req.body.Email
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
       });
-    
-    res.status(201).json(newUser);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Error creating user');
+      res.status(201).json(newUser);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error: ' + error);
   }
 });
 
@@ -186,7 +135,7 @@ app.put('/users/:Username', passport.authenticate('jwt', {session: false}), [
   check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
   check('Password', 'Password is required').not().isEmpty(),
 ], async (req, res) => {
-  if (req.user.Username !== req.params.Username) {
+  if (req.user.Usernam !== req.params.Username) {
     return res.status(400).send('Permision denied.');
   }
   const errors = validationResult(req);
@@ -259,6 +208,7 @@ app.delete('/users/:Username/movies/:MovieID', passport.authenticate('jwt', {ses
     res.status(500).send('Error: ' + error);
   }
 });
+
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
