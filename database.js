6b22a1014ecca3cac30eb61fcda9a3416e086db5
Bob3x <bob3xx@protonmail.com>
2024-12-06 16:20:57 +0200

    Update mongoose connection and documentation

--- database.js
+++ database.js
@@ -2,7 +2,7 @@
  * @module MongoDBConnection
  * @description Database connection configuration and model export
  * @requires mongoose
- * @requires ./models
+ * @requires ./models - Database schema definitions
  */
 
 /**
@@ -12,15 +12,31 @@ const mongoose = require("mongoose");
 const Models = require("./models.js");
 
 /**
- * @constant {Model} Movies - Movie model reference
- * @constant {Model} Users - User model reference
+ * Movie and User model references
+ * @constant {Model} Movies - Movie collection model
+ * @constant {Model} Users - User collection model
+ * @example
+ * // Using Movies model
+ * const movie = await Movies.findOne({ Title: "Movie Name" });
+ *
+ * // Using Users model
+ * const user = await Users.findOne({ Username: "username" });
  */
 const Movies = Models.Movie;
 const Users = Models.User;
 
-// Local server database connection
-// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// mongoose.connect('process.env.CONNECTION_URI', { useNewUrlParser: true, useUnifiedTopology: true });
+/**
+ * MongoDB connection URI
+ * @constant {string} uri - MongoDB connection string
+ * @default mongodb://localhost:27017/myFlixDB
+ * @example
+ * // Local connection
+ * mongodb://localhost:27017/myFlixDB
+ *
+ * // Atlas connection
+ * mongodb+srv://<username>:<password>@cluster.mongodb.net/myFlixDB
+ */
+// const uri = process.env.CONNECTION_URI || "mongodb://localhost:27017/myFlixDB";
 
 /**
  * MongoDB Atlas connection configuration
@@ -30,25 +46,33 @@ const Users = Models.User;
  */
 const username = encodeURIComponent("bob3x");
 const password = encodeURIComponent("lEX6W7KWRvmEKiTL");
-const uri = `mongodb+srv://${username}:${password}@my-db-cluster.2utmo.mongodb.net/myFlixDB?retryWrites=true&w=majority&appName=my-DB-cluster`;
+const uri = `mongodb+srv://${username}:${password}@my-db-cluster.2utmo.mongodb.net/myFlixDB?retryWrites=true&w=majority&appName=my-DB-cluster`;*/
 
 /**
  * Establish MongoDB connection
  * @function connect
+ * @throws {Error} Connection error
  * @returns {Promise} Mongoose connection promise
- * @throws {Error} Database connection error
  */
 mongoose
-    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Error connecting to MongoDB:", err));
+    .connect(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to MongoDB:", uri.includes("localhost") ? "Local" : "Atlas");
+    })
+    .catch((error) => {
+        console.error("MongoDB connection error:", error);
+        process.exit(1);
+    });
 
 /**
- * @exports {Object} Database models
- * @property {Model} Movies - Movie model
- * @property {Model} Users - User model
+ * @exports Database models
+ * @property {Model} Movies - Movie model for CRUD operations
+ * @property {Model} Users - User model for CRUD operations
  */
 module.exports = {
-    Movies: Models.Movie,
-    Users: Models.User
+    Movies,
+    Users
 };
